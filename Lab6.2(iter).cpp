bcf4328a8c85203cbe4517c2135748d084a39aa0
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-11-15 18:48:20 +0200

    Initial commit

---
+++ Lab6.2(iter).cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <time.h>
+#include <iomanip>
+
+using namespace std;
+
+void Create(int* c, const int size, int min, int max) {
+	for (int i = 0; i < size; i++) {
+		c[i] = min + rand() % (max - min + 1);
+	}
+}
+
+void arrPrint(int* c, const int size) {
+	for (int i = 0; i < size; i++) {
+		cout << setw(4) << c[i];
+	}
+	cout << endl;
+}
+
+int arrMaxMin(int* c, const int size, int max, int min, int& indexMax, int& indexMin) {
+	for (int i = 1; i < size; i++) {
+		if (c[i] > max) {
+			max = c[i];
+			indexMax = i;
+		}
+		if (c[i] < min) {
+			min = c[i];
+			indexMin = i;
+		}
+	}
+	return indexMax;
+}
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	const int a = 15;
+
+	int indexMax = 0, indexMin = 0;
+
+	int c[a];
+
+	int Low = 5;
+	int High = 90;
+
+	Create(c, a, Low, High);
+	arrPrint(c, a);
+	arrMaxMin(c, a, c[0], c[0], indexMax, indexMin);
+	cout << "indexMax = " << indexMax << endl;
+	cout << "indexMin = " << indexMin << endl;
+
+
+	system("pause");
+	cin.get();
+	return 0;
+
+}
+
---
+++ Lab6.2(iter)Test/Lab6.2(iter)Test.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.2(iter).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab62iterTest
+{
+	TEST_CLASS(Lab62iterTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t, indexMax = 0, indexMin = 0;
+			const int a = 5;
+			int c[a] = { -5, 10, 2, 3, 10 };
+			t = arrMaxMin(c, a, c[0], c[0], indexMax, indexMin);
+			Assert::AreEqual(t, 1);
+			
+		}
+	};
+}
